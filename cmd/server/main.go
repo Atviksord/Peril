ada31755a6b4e0181a1995867610d67ed64d6f99
Atviksord <Atviksord@gmail.com>
2024-12-16 22:14:40 +0900

    d

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672"
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Println("Couldnt establish connection", err)
+	}
+
+	defer connection.Close()
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt)
+	<-signalChannel
+	fmt.Println("Shutting down")
+
 }
