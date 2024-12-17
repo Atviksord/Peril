773e215fa6d4cb358c7149651e1ef111114884af
Atviksord <Atviksord@gmail.com>
2024-12-17 20:29:58 +0900

    small update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,9 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -17,6 +20,15 @@ func main() {
 	}
 
 	defer connection.Close()
+
+	connectionChannel, err := connection.Channel()
+
+	if err != nil {
+		fmt.Printf("Error getting connection channel of type AMPQP Channel")
+	}
+
+	pubsub.PublishJSON(connectionChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+
 	signalChannel := make(chan os.Signal, 1)
 	signal.Notify(signalChannel, os.Interrupt)
 	<-signalChannel
---
+++ internal/pubsub/pubsub.go
@@ -0,0 +1,33 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+
+}
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+
+	jsonedValue, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+	err = ch.PublishWithContext(context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        jsonedValue,
+		},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
